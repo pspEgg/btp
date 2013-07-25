55fef35190446ce7edc93dd8965d06fa9b3bb90b
Dan Liu <skipkinggg@gmail.com>
2013-07-25 03:13:19 -0400

    css tweaks

--- _plugins/words.rb
+++ _plugins/words.rb
@@ -5,7 +5,7 @@ module Jekyll
   class WordsDataGenerator < Generator
     safe true
     def generate(site)
-      quotes_text = File.read(File.join(site.source, 'btp_quotes.txt'))
+      quotes_text = File.read(File.join(site.source, 'btp_quotes_dev.txt'))
       parser = BtpParser.new
       quotes_array = parser.parse(quotes_text)
       site.config['quotes'] = quotes_array
